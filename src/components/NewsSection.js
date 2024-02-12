416a0582cad20a5be8792fd9c2ea3155fa0dbb7b
martinromario55 <martinromario55@gmail.com>
2024-02-12 10:07:40 +0300

    Finish up Home Screen - Breaking News, Recommeded News, Bookmarks

---
+++ src/components/NewsSection.js
@@ -0,0 +1,155 @@
+import {
+  FlatList,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View
+} from 'react-native'
+import React, { useCallback, useEffect, useState } from 'react'
+import { useFocusEffect, useNavigation } from '@react-navigation/native'
+import { heightPercentageToDP as hp } from 'react-native-responsive-screen'
+import { BookmarkSquareIcon } from 'react-native-heroicons/outline'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+const NewsSection = ({ newsProps }) => {
+  const navigation = useNavigation()
+  const [bookmarkStatus, setBookmarkStatus] = useState([])
+  const [urlList, setUrlList] = useState([])
+
+  const handleClick = (item) => {
+    navigation.navigate('NewsDetailsScreen', { item })
+  }
+
+  useEffect(() => {
+    const urls = newsProps.map((item) => item.url)
+    setUrlList(urls)
+  }, [newsProps])
+
+  const toggleBookmarkandSave = async (item, index) => {
+    try {
+      const saveArticles = await AsyncStorage.getItem('saveArticles')
+      let savedArticlesArray = saveArticles ? JSON.parse(saveArticles) : []
+
+      const isArticleBookmarked = savedArticlesArray.some(
+        (saveArticles) => saveArticles.url === item.url
+      )
+
+      if (!isArticleBookmarked) {
+        savedArticlesArray.push(item)
+        await AsyncStorage.setItem(
+          'saveArticles',
+          JSON.stringify(savedArticlesArray)
+        )
+        const updatedStatus = [...bookmarkStatus]
+        updatedStatus[index] = true
+        setBookmarkStatus(updatedStatus)
+      } else {
+        const updatedSavedArticlesArray = savedArticlesArray.filter(
+          (savedArticle) => savedArticle.url !== item.url
+        )
+
+        await AsyncStorage.setItem(
+          'saveArticles',
+          JSON.stringify(updatedSavedArticlesArray)
+        )
+        const updatedStatus = [...bookmarkStatus]
+        updatedStatus[index] = false
+        setBookmarkStatus(updatedStatus)
+      }
+    } catch (error) {
+      console.log('Error saving news', error)
+    }
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      const loadSavedArticles = async () => {
+        try {
+          const savedArticles = await AsyncStorage.getItem('saveArticles')
+          const savedArticlesArray = savedArticles
+            ? JSON.parse(savedArticles)
+            : []
+
+          const isArticleBookmarkedList = urlList.map((url) =>
+            savedArticlesArray.some((savedArticle) => savedArticle.url === url)
+          )
+          setBookmarkStatus(isArticleBookmarkedList)
+        } catch (error) {
+          console.log('Error loading saved articles', error)
+        }
+      }
+      loadSavedArticles()
+    }, [urlList, navigation])
+  )
+
+  const renderItem = ({ item, index }) => {
+    return (
+      <TouchableOpacity
+        className="mb-4 mx-4 space-y-1"
+        key={index}
+        onPress={() => handleClick(item)}
+      >
+        {/* Image */}
+        <View className="flex-row justify-start w-[100%] shadow-sm">
+          <Image
+            source={{
+              uri: item.urlToImage || 'https://via.placeholder.com/200x200'
+            }}
+            style={{
+              width: hp(9),
+              height: hp(10)
+            }}
+          />
+
+          {/* Author */}
+          <View className="w-[70%] pl-4 justify-center space-y-1">
+            <Text className="text-xs font-bold text-gray-900 dark:text-neutral-300">
+              {item?.author?.length > 20
+                ? item?.author?.slice(0, 20) + '...'
+                : item?.author}
+            </Text>
+
+            {/* Title */}
+            <Text className="text-neutral-800 capitalize max-w-[90%] dark:text-white">
+              {item?.title?.length > 50
+                ? item?.title?.slice(0, 50) + '...'
+                : item?.title}
+            </Text>
+
+            {/* Date */}
+            <Text className="text-xs text-gray-700 dark:text-neutral-300">
+              {new Date(item?.publishedAt).toDateString()}
+            </Text>
+          </View>
+          {/* Bookmark icon */}
+          <View className="w-[10%] justify-center">
+            <TouchableOpacity
+              onPress={() => toggleBookmarkandSave(item, index)}
+            >
+              <BookmarkSquareIcon
+                color={bookmarkStatus[index] ? 'green' : 'gray'}
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </TouchableOpacity>
+    )
+  }
+  return (
+    <View className="space-y-2 bg-white dark:bg-neutral-900">
+      <FlatList
+        nestedScrollEnabled={true}
+        scrollEnabled={false}
+        data={newsProps}
+        showsVerticalScrollIndicator={false}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={renderItem}
+      />
+    </View>
+  )
+}
+
+export default NewsSection
+
+const styles = StyleSheet.create({})
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,4 +1,11 @@
-import { SafeAreaView, StyleSheet, Text, View, StatusBar } from 'react-native'
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  ScrollView
+} from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { useColorScheme } from 'nativewind'
 import { useQuery } from '@tanstack/react-query'
@@ -7,12 +14,16 @@ import Header from '../components/Header'
 import Loading from '../components/Loading'
 import MiniHeader from '../components/MiniHeader'
 import BreakingNews from '../components/BreakingNews'
+import { heightPercentageToDP as hp } from 'react-native-responsive-screen'
+import NewsSection from '../components/NewsSection'
 
 const HomeScreen = () => {
   const { colorScheme, toggleColorScheme } = useColorScheme()
   const [breakingNews, setBreakingNews] = useState([])
-  const [recommendedNews, setRecommentedNews] = useState([])
   const [isLoading, setIsLoading] = useState(false)
+  const [recommendedNews, setRecommentedNews] = useState([])
+  const [isLoadingRecommendedNews, setIsLoadingRecommendedNews] =
+    useState(false)
 
   // Breaking News
   // const { isLoading: isBreakingNewsLoading } = useQuery({
@@ -32,42 +43,72 @@ const HomeScreen = () => {
       setIsLoading(true)
       fetchBreakingNews().then((data) => {
         setBreakingNews(data.articles)
+        setIsLoading(false)
       })
-      setIsLoading(false)
     } catch (error) {
       console.log('Error fetching breaking news:', error)
-      setIsLoading(false)
+      setIsLoading(true)
     }
   }, [])
 
   // console.log('breakingnews:', breakingNews)
 
   // Recommeded News
-  const { isLoading: isRecommendedNewsLoading } = useQuery({
-    queryKey: ['recommendedNews'],
-    queryFn: fetchRecommendedNews,
-    onSuccess: (data) => {
-      setBreakingNews(data.articles)
-    },
-    onError: (error) => {
+  // const { isLoading: isRecommendedNewsLoading } = useQuery({
+  //   queryKey: ['recommendedNews'],
+  //   queryFn: fetchRecommendedNews,
+  //   onSuccess: (data) => {
+  //     setBreakingNews(data.articles)
+  //   },
+  //   onError: (error) => {
+  //     console.log('Error fetching recommended news:', error)
+  //   }
+  // })
+  // use Effect Recommeded News
+  useEffect(() => {
+    try {
+      setIsLoadingRecommendedNews(true)
+      fetchRecommendedNews().then((data) => {
+        setRecommentedNews(data.articles)
+        setIsLoadingRecommendedNews(false)
+      })
+    } catch (error) {
       console.log('Error fetching recommended news:', error)
+      setIsLoadingRecommendedNews(false)
     }
-  })
+  }, [])
 
   return (
-    <SafeAreaView>
+    <SafeAreaView className="flex-1 bg-white dark:bg-neutral-900">
       <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} />
-      <Header />
+      <View>
+        <Header />
 
-      {isLoading ? (
-        <Loading />
-      ) : (
+        {isLoading ? (
+          <Loading />
+        ) : (
+          <View>
+            <MiniHeader label="Breaking News" />
+
+            <BreakingNews label={'breakingNews'} data={breakingNews} />
+          </View>
+        )}
+
         <View>
-          <MiniHeader label="Breaking News" />
+          <MiniHeader label="Recommended News" />
 
-          <BreakingNews label={'breakingNews'} data={breakingNews} />
+          <ScrollView contentContainerStyle={{ paddingBottom: hp(80) }}>
+            {isLoadingRecommendedNews ? (
+              <Loading />
+            ) : (
+              <NewsSection
+                label="RecommedadedNews"
+                newsProps={recommendedNews}
+              />
+            )}
+          </ScrollView>
         </View>
-      )}
+      </View>
     </SafeAreaView>
   )
 }
